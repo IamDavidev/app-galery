4278956ab0f0a2f9ec61631e07487432e9dccaf9
L-David-26 <davidlez2mgk@gmail.com>
2021-10-03 18:08:43 -0500

    footer add co

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+
+
+const Footer = ()=>{
+    return (
+        <div className="footer">
+            <h1>
+        david Lezama 
+            </h1>
+            <div>
+    )
+}
