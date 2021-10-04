67d60c4816fb4498d96366e3f0df940b088d2597
L-David-26 <davidlez2mgk@gmail.com>
2021-10-04 00:07:57 -0500

    Footer programming

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Photos from './components/Photos';
 import './index.css';
-
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div className="App__root has-background-black">
       <NavBar />
       <Photos />
+      <Footer />
     </div>
   );
 };
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,11 @@
-import react from 'react'
+import react from 'react';
 
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1>david Lezama</h1>
+    </div>
+  );
+};
 
-const Footer = ()=>{
-    return (
-        <div className="footer">
-            <h1>
-        david Lezama 
-            </h1>
-            <div>
-    )
-}
+export default Footer;
